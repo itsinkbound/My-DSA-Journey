8ceede883af38829bc464ff0ca639a5013345422
Krishna Priyadarshan Behara <kpbehara10@gmail.com>
2026-05-19 17:28:45 +0530

    Add algorithm comments for moving negatives
    Add comments explaining the algorithm to move negative numbers.

---
+++ arrays/move-all-negative-to-side.java
@@ -0,0 +1,7 @@
+//can be done using two pointers to compare and shift
+// while left < right
+// if the left is neg left++
+// if there are elements left and right is pos then right --
+// otherewise swap left and right 
+
+// BUT this does not preserve the order so if order is required what we do is we simply store all the positicve and then append them ahead

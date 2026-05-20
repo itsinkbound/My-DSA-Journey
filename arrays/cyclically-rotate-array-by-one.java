4e64d0e17d6a9886ba9dfb715acc5914b5492355
Krishna Priyadarshan Behara <kpbehara10@gmail.com>
2026-05-20 16:07:16 +0530

    Add comments for array rotation logic
    Add comments explaining the logic for rotating an array.

---
+++ arrays/cyclically-rotate-array-by-one.java
@@ -0,0 +1,2 @@
+//The basic idea is to store the last element in a temp variable and shift every other element one position ahead. 
+//After shifting, update the first element with value stored in temp.
